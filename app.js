b4d9742c326f2da2c3a56c732de9aa03e05ec0bc
Steven Myers <slmyers@ualberta.ca>
2016-05-12 16:22:35 -0600

    able to submit to server

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express'),
-      path    = require('path'),
-      app     = express(),
-      test    = require('./questionaire');
+const express     = require('express'),
+      path        = require('path'),
+      bodyParser  = require('body-parser')
+      app         = express(),
+      test        = require('./questionaire');
 
-
+app.use(bodyParser.json())
 app.use(express.static(__dirname));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/static/html/index.html'));
@@ -12,7 +13,10 @@ app.get('/test', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(test));
 });
-
+app.post('/submit_handler', function (req, res) {
+  console.log(req.body);
+  res.send(JSON.stringify({success: true}));
+});
 
 app.listen(3000, "0.0.0.0", function () {
   console.log('Example app listening on port 3000!');
--- static/js/loadData.js
+++ static/js/loadData.js
@@ -1,5 +1,16 @@
 (function () {
   document.addEventListener("DOMContentLoaded", function(event) {
     vsaq.qpageObject_.loadQuestionnaire("test");
+    var sub_btn = goog.dom.getElement('_vsaq_submit_questionnaire');
+    goog.events.listen(sub_btn,goog.events.EventType.CLICK,
+      function() {
+        console.log(vsaq.qpageObject_.questionnaire.getValuesAsJson())
+        var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+        xmlhttp.open("POST", "/submit_handler");
+        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xmlhttp.send(vsaq.qpageObject_.questionnaire.getValuesAsJson());
+      }
+    );
+
   });
 })();
