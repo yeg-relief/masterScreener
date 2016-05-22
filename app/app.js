dc9d1f3e73c797c51f3dfa1ad804ce900e359ea0
Steven Myers <slmyers@ualberta.ca>
2016-05-22 01:52:02 -0600

    Frontend (#3)
    * moved load data inline
    
    * webpack and npm build
    
    * retrying rx stuff
    
    * finally figured out how to build
    
    * able to get response from submission
    
    * render response after submission

---
+++ app/app.js
@@ -0,0 +1,32 @@
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/dom/ajax';
+import {AsyncSubject} from 'rxjs/AsyncSubject';
+
+const
+ready       = Observable.fromEvent(document, 'DOMContentLoaded'),
+submitBtn   = Observable.fromEvent(goog.dom.getElement('_vsaq_submit_questionnaire'),
+                goog.events.EventType.CLICK);
+
+
+const readySub = ready.subscribe( () => {
+  vsaq.qpageObject_.loadQuestionnaire("masterScreener");
+  readySub.unsubscribe();
+});
+const submitSub = submitBtn.subscribe( () => {
+  var obj = {
+    url: '/masterSubmit',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: vsaq.qpageObject_.questionnaire.getValuesAsJson(),
+    resultSelector: function (res) { return res.response.questionnaire; }
+  };
+  Observable
+  .ajax(obj)
+  .subscribe( x => {
+    vsaq.qpageObject_.questionnaire.setTemplate(x);
+    vsaq.qpageObject_.questionnaire.render();
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/*
+  fairly brittle webpack configuration, but it get's the job done.
+*/
+module.exports = {
+  context: __dirname + "/app",
+  entry: "./app",
+  output: {
+    path: __dirname + "/static" +"/js",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: __dirname + "/app"
+      }
+    ]
+  }
+}
