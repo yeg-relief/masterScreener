582deb3d2d433179f292b8a7ad9f7db7d35efb96
Steven Myers <slmyers@ualberta.ca>
2016-05-23 00:22:47 -0600

    simple client side cache

--- app/app.js
+++ app/app.js
@@ -2,47 +2,62 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/dom/ajax';
 
-let masterTemplate;
 const
 submitBtn  = document.getElementById('_vsaq_submit_questionnaire'),
 clearBtn   = document.getElementById('_vsaq_clear_questionnaire'),
 returnBtn  = document.getElementById('return_to_master'),
 masterBtns = [submitBtn, clearBtn],
-resultBtns = [returnBtn];
+resultBtns = [returnBtn],
+cache = new Map();
 
 Observable.fromEvent(document, 'DOMContentLoaded')
 .subscribe( () => {
   vsaq.qpageObject_.loadQuestionnaire("masterScreener");
 });
 
-Observable.fromEvent(submitBtn, 'click')
-.subscribe( () => {
+const netCall = () => {
+  const answers = vsaq.qpageObject_.questionnaire.getValuesAsJson();
+  if (cache[answers]) {
+    console.log(`from cached answer`);
+    cache['masterTemplate'] = vsaq.qpageObject_.questionnaire.getTemplate();
+    vsaq.qpageObject_.questionnaire.setTemplate(cache[answers]);
+    vsaq.qpageObject_.questionnaire.render();
+    toggleUiBtns();
+    return;
+  }
   const ajaxSettings = {
     url: '/masterSubmit',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: vsaq.qpageObject_.questionnaire.getValuesAsJson(),
+    body: answers,
     resultSelector: function (res) { return res.response.questionnaire; }
   };
   Observable
-  .ajax(ajaxSettings)
-  .subscribe( x => {
-    masterTemplate = vsaq.qpageObject_.questionnaire.getTemplate();
-    vsaq.qpageObject_.questionnaire.setTemplate(x);
-    vsaq.qpageObject_.questionnaire.render();
-    toggleUiBtns();
-  });
-});
+    .ajax(ajaxSettings)
+    .subscribe( res => {
+      console.log(`from network call`);
+      cache['masterTemplate'] = vsaq.qpageObject_.questionnaire.getTemplate();
+      vsaq.qpageObject_.questionnaire.setTemplate(res);
+      vsaq.qpageObject_.questionnaire.render();
+      toggleUiBtns();
+      cache[answers] = res;
+    });
+}
 
+
+Observable.fromEvent(submitBtn, 'click')
+.subscribe( () => {
+  netCall();
+});
 Observable.fromEvent(returnBtn, 'click')
 .subscribe( () => {
-  if (!masterTemplate) {
+  if (!cache['masterTemplate']) {
     alert('masterTemplate is falsey');
     console.log(`masterTemplate: \n ${masterTemplate}`);
   } else {
-    vsaq.qpageObject_.questionnaire.setTemplate(masterTemplate);
+    vsaq.qpageObject_.questionnaire.setTemplate(cache['masterTemplate']);
     // Load answers from localStorage (if available).
     const storageData = vsaq.qpageObject_.readStorage_();
     if (storageData) {
